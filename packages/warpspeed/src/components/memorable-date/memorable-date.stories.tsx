1d9ec4d080e5aeb75b960b346b3cd613983345ba
Amber Kolar <amberj.kolar@gmail.com>
2023-05-02 00:14:31 -0700

    Linter and formatter

--- packages/warpspeed/src/components/memorable-date/memorable-date.stories.tsx
+++ packages/warpspeed/src/components/memorable-date/memorable-date.stories.tsx
@@ -5,15 +5,17 @@ import { MemorableDateProps } from './memorable-date';
 
 const meta: Meta<typeof MemorableDate> = {
   title: 'USWDS/Forms/Memorable Date',
-  component: MemorableDate
+  component: MemorableDate,
 };
 export default meta;
 
-const Template: StoryFn<typeof MemorableDate> = (args: MemorableDateProps) => <MemorableDate {...args} />;
+const Template: StoryFn<typeof MemorableDate> = (args: MemorableDateProps) => (
+  <MemorableDate {...args} />
+);
 
 export const Standard = Template.bind({});
 Standard.args = {
-  id: '1'
+  id: '1',
 };
 
 export const DefaultValue = Template.bind({});
@@ -21,5 +23,5 @@ DefaultValue.args = {
   id: '2',
   defaultMonthValue: 12,
   defaultDayValue: 30,
-  defaultYearValue: 2000
+  defaultYearValue: 2000,
 };
--- packages/warpspeed/src/components/memorable-date/memorable-date.test.tsx
+++ packages/warpspeed/src/components/memorable-date/memorable-date.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import /* userEvent, */ { render, screen /* fireEvent */} from '@testing-library/react';
+import { /* userEvent, */ render, screen /* fireEvent */ } from '@testing-library/react';
 import { MemorableDate } from './memorable-date';
 
 describe('Memorable Date', () => {
