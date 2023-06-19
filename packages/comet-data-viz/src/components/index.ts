d23cc38b8b55536f81d3f33ab5155a91ee07c239
Jonathan Bouder <jbouder@metrostar.com>
2023-06-19 10:58:43 -0400

    Add scatter graph to data viz package.

--- packages/comet-data-viz/src/components/index.ts
+++ packages/comet-data-viz/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as LineGraph } from './line-graph';
 export { default as AreaGraph } from './area-graph';
 export { default as PolarAxis } from './polar-axis';
 export { default as PieChart } from './pie-chart';
+export { default as ScatterGraph } from './scatter-graph';
---
+++ packages/comet-data-viz/src/components/scatter-graph/scatter-graph.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { VictoryChart, VictoryScatter } from 'victory';
+import ChartProps from '../chart/chart';
+
+export interface ScatterGraphProps {
+  /**
+   * An object providing chart specific attributes
+   */
+  chart: ChartProps;
+  /**
+   * Determines how to scale each data point
+   */
+  size?: number;
+  /**
+   * An string array of color values used to colorize chart sections
+   */
+  colors?: string[];
+  /**
+   * An array of data points
+   */
+  data: Array<
+    Array<{
+      x: number;
+      y: number;
+    }>
+  >;
+}
+
+/**
+ * Renders a dataset as a single line path.
+ */
+const ScatterGraph: React.FC<ScatterGraphProps> = (props: ScatterGraphProps) => {
+  return (
+    <VictoryChart {...props.chart}>
+      {props.data.map((d, i) => (
+        <VictoryScatter
+          key={`scatter-${i}`}
+          data={d}
+          size={props.size}
+          //   style={{ data: { fill: props.color } }}
+          style={{
+            data: {
+              fill: props.colors
+                ? props.colors[i]
+                  ? props.colors[i]
+                  : props.colors[0]
+                : '#0d7ea2',
+            },
+          }}
+          animate={{
+            duration: 2000,
+            onLoad: { duration: 1000 },
+          }}
+        />
+      ))}
+    </VictoryChart>
+  );
+};
+
+export default ScatterGraph;
