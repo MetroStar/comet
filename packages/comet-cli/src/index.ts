85a8f1f4ddb2f5177b630124707c797b9abb1e96
Johnny Bouder <jbouder@metrostar.com>
2024-01-09 09:46:33 -0500

    Add command for creating api from comet cli.

--- packages/comet-cli/src/index.ts
+++ packages/comet-cli/src/index.ts
@@ -2,7 +2,7 @@ import { Command } from 'commander';
 import figlet from 'figlet';
 import fs from 'fs';
 import { exec } from 'child_process';
-import { REPO_URL } from './constants';
+import { API_REPO_URL, UI_REPO_URL } from './constants';
 
 const log = console.log;
 const error = console.error;
@@ -13,9 +13,9 @@ log(figlet.textSync('Comet CLI'));
 log('');
 
 program
-  .command('init')
-  .description('Create a new app from the Comet Stater App')
-  .option('-n, --name <string>', 'project/directory name')
+  .command('ui')
+  .description('Create a new app from the Comet Starter App')
+  .option('-n, --name <string>', 'project/directory name', 'comet-app')
   .action((options) => {
     const projectName = options.name;
     log(`Creating a new Comet app with name: ${projectName}`);
@@ -28,8 +28,34 @@ program
     fs.mkdirSync(projectName);
     log('Cloning starter app into project...');
 
-    const command = `git clone ${REPO_URL} ${projectName}`;
+    const command = `git clone ${UI_REPO_URL} ${projectName}`;
     exec(command, (err: any) => {
+      if (err) {
+        error(`Error: ${err}`);
+      } else {
+        log('Project initialized successfully!');
+      }
+    });
+  });
+
+program
+  .command('api')
+  .description('Create a new api from the Comet API')
+  .option('-n, --name <string>', 'project/directory name', 'comet-api')
+  .action((options) => {
+    const projectName = options.name;
+    log(`Creating a new Comet api with name: ${projectName}`);
+    if (fs.existsSync(projectName)) {
+      log('Directory already exists!');
+      process.exit(1);
+    }
+
+    log('Initializing project directory...');
+    fs.mkdirSync(projectName);
+    log('Cloning api into project...');
+
+    const command = `git clone ${API_REPO_URL} ${projectName}`;
+    exec(command, (err) => {
       if (err) {
         error(`Error: ${err}`);
       } else {
