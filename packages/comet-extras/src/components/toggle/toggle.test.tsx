c85337513e83d8d259e4fc0a78757c703b4c9c45
kildre <kildre@gmail.com>
2024-01-31 11:45:34 -0500

    comet-ui-toggle-comp # added A11Y test in component

--- packages/comet-extras/src/components/toggle/toggle.test.tsx
+++ packages/comet-extras/src/components/toggle/toggle.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { axe } from 'jest-axe';
 import { Toggle } from './toggle'; // Assuming the Toggle component is in the same directory
 
 describe('Toggle Component Tests', () => {
+  test('should render with no accessibility violations', async () => {
+    const { container } = render(
+      <Toggle id="test-toggle" label="Test toggle" onChange={jest.fn()} />,
+    );
+    expect(await axe(container)).toHaveNoViolations();
+  });
   // Test to check if the Toggle renders correctly
   test('renders the Toggle component', () => {
     render(<Toggle id="test-toggle" />);
