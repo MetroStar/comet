738ff721d62404590e0248a625ccfb276b55a602
jbouder <61591423+jbouder@users.noreply.github.com>
2024-06-18 11:57:33 +0000

    Deploying to gh-pages from @ MetroStar/comet@dbbfc1bf98ddb20c74f486ccc19a234370535e7a 🚀

---
+++ assets/date-range-picker.stories-D8RJthTd.js
@@ -0,0 +1,21 @@
+import{j as e}from"./jsx-runtime--26OcTxz.js";import{D as o,i as a}from"./time-picker-tSMyvaNy.js";import"./banner-BIrNeGaP.js";import"./button-B1UM48ou.js";import"./button-group-Sj3TVDAs.js";import"./checkbox-Bjoz-qBs.js";import"./combo-box-BjT8FolL.js";import{D as r}from"./date-picker-B-g1KeMn.js";import"./error-messages-DmkQ7jqj.js";import"./file-input-BvZTU4-J.js";import"./icon-BIwXrJ_M.js";import{L as n}from"./label-BySvIC3s.js";import"./modal-DNjy2LZo.js";import"./process-list-CrzgXWd-.js";import"./radio-button-DHw21JsY.js";import"./range-slider-CirwSfdS.js";import"./search-CtLVkVWT.js";import"./select-BOzcq5T1.js";import"./side-navigation-B34iFLMf.js";import"./site-alert-DrW0OowE.js";import"./step-indicator-J2kyw9Jo.js";import"./summary-box-fohymN9-.js";import"./tag-BjNh5Gwy.js";import"./text-area-XQEmcgpC.js";import"./tooltip-B9ueSJ9K.js";import"./index-BLgStI3F.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Bl6ORisp.js";import"./config-BXli1stc.js";import"./select-or-matches-Bu-g4rUn.js";import"./sprite-Cw9lcZJJ.js";import"./events-C6LRLXdn.js";import"./sanitizer-BtXph-xc.js";import"./is-in-viewport-B5ZZJcwh.js";import"./index-D6IA0wpC.js";import"./active-element-CTMAq7ve.js";const K={title:"USWDS/Forms/Date Range Picker",component:o},s=()=>e.jsxs(o,{id:"date-range-picker",children:[e.jsxs(a,{children:[e.jsx(n,{id:"event-date-start-label",htmlFor:"event-date-start",children:"Event start date"}),e.jsx("div",{className:"usa-hint",id:"event-date-start-hint",children:"mm/dd/yyyy"}),e.jsx(r,{id:"event-date-start",name:"event-date-start","aria-describedby":"event-date-start-label event-date-start-hint"})]}),e.jsxs(a,{children:[e.jsx(n,{id:"event-date-end-label",htmlFor:"event-date-end",children:"Event end date"}),e.jsx("div",{className:"usa-hint",id:"event-date-end-hint",children:"mm/dd/yyyy"}),e.jsx(r,{id:"event-date-end",name:"event-date-end","aria-describedby":"event-date-end-label event-date-end-hint"})]})]}),t=s.bind({});var d,i,m;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`() => <DateRangePickerContainer id="date-range-picker">
+    <FormGroup>
+      <Label id="event-date-start-label" htmlFor="event-date-start">
+        Event start date
+      </Label>
+      <div className="usa-hint" id="event-date-start-hint">
+        mm/dd/yyyy
+      </div>
+      <DatePicker id="event-date-start" name="event-date-start" aria-describedby="event-date-start-label event-date-start-hint" />
+    </FormGroup>
+
+    <FormGroup>
+      <Label id="event-date-end-label" htmlFor="event-date-end">
+        Event end date
+      </Label>
+      <div className="usa-hint" id="event-date-end-hint">
+        mm/dd/yyyy
+      </div>
+      <DatePicker id="event-date-end" name="event-date-end" aria-describedby="event-date-end-label event-date-end-hint" />
+    </FormGroup>
+  </DateRangePickerContainer>`,...(m=(i=t.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const M=["Standard"];export{t as Standard,M as __namedExportsOrder,K as default};
