65cb3200f3d52e674aa4c4b86ec324a7bc2c5f49
Johnny Bouder <jbouder@metrostar.com>
2024-07-16 14:06:59 -0400

    Add figma code connect configs. Add basic card figma example.

---
+++ packages/comet-uswds/src/components/card/card.figma.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Card, CardBody, CardFooter, CardHeader } from './card';
+import figma from '@figma/code-connect';
+import Button from '../button';
+
+figma.connect(
+  Card,
+  'https://www.figma.com/design/U58Pbb84dLaZfvFvdtGVdT/Comet-UI-Kit?node-id=43-2604&t=gbmwd0bpvth9DyiO-4',
+  {
+    props: {
+      cardimage430: figma.boolean('Card image?'),
+    },
+    example: (props) => (
+      <Card id="card-1">
+        <CardHeader>Card Header</CardHeader>
+        <CardBody>The meat of the card...</CardBody>
+        <CardFooter>
+          <Button id="card-button" variant="outline">
+            Click me...
+          </Button>
+        </CardFooter>
+      </Card>
+    ),
+  },
+);
