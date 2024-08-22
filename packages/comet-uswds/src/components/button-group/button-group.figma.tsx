1b05c0d17b8955343a93dcef36909a2f99d40377
Johnny Bouder <jbouder@metrostar.com>
2024-08-22 08:17:49 -0400

    Update button group code connect to provide multiple variants.

--- packages/comet-uswds/src/components/button-group/button-group.figma.tsx
+++ packages/comet-uswds/src/components/button-group/button-group.figma.tsx
@@ -6,19 +6,44 @@ figma.connect(
   ButtonGroup,
   'https://www.figma.com/design/U58Pbb84dLaZfvFvdtGVdT/Comet-UI-Kit?node-id=31-1897',
   {
-    props: {
-      type: figma.enum('Type', {
-        Default: 'default',
-        Segmented: 'segmented',
-      }),
+    props: {},
+    variant: {
+      Type: 'Default',
     },
-    example: ({ type }) => {
+    example: () => {
       const handleClick = () => {
         // Do something
       };
 
       return (
-        <ButtonGroup id="button-group" type={type}>
+        <ButtonGroup id="button-group" type="default">
+          <Button id="button-1" onClick={handleClick} variant="outline">
+            Back
+          </Button>
+          <Button id="button-2" onClick={handleClick} variant="default">
+            Continue
+          </Button>
+        </ButtonGroup>
+      );
+    },
+  },
+);
+
+figma.connect(
+  ButtonGroup,
+  'https://www.figma.com/design/U58Pbb84dLaZfvFvdtGVdT/Comet-UI-Kit?node-id=31-1897',
+  {
+    props: {},
+    variant: {
+      Type: 'Segmented',
+    },
+    example: () => {
+      const handleClick = () => {
+        // Do something
+      };
+
+      return (
+        <ButtonGroup id="button-group" type="segmented">
           <Button id="button-1" onClick={handleClick} variant="default">
             Map
           </Button>
