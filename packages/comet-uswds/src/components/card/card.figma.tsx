d3160856d06fdfdda56e90c7946dcc5d0f75e11e
Johnny Bouder <jbouder@metrostar.com>
2024-08-27 16:34:34 -0400

    Update card code connect file for card updates.

--- packages/comet-uswds/src/components/card/card.figma.tsx
+++ packages/comet-uswds/src/components/card/card.figma.tsx
@@ -1,20 +1,116 @@
-import { Card, CardBody, CardFooter, CardHeader } from './card';
+import { Card, CardBody, CardFooter, CardHeader, CardMedia } from './card';
 import figma from '@figma/code-connect';
 import Button from '../button';
+
+figma.connect(
+  Card,
+  'https://www.figma.com/design/U58Pbb84dLaZfvFvdtGVdT/Comet-UI-Kit?node-id=43-2601',
+  {
+    props: {
+      layout: figma.enum('Type', {
+        Default: 'default',
+        Flag: 'flag',
+      }),
+      inset: figma.enum('Variant', {
+        Inset: true,
+      }),
+      exdent: figma.enum('Variant', {
+        Exdent: true,
+      }),
+    },
+    example: ({ layout, inset, exdent }) => (
+      <Card id="card-default" layout={layout}>
+        <CardMedia inset={inset} exdent={exdent}>
+          <img
+            src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg"
+            alt="a placeholder image"
+          />
+        </CardMedia>
+      </Card>
+    ),
+  },
+);
 
 figma.connect(
   Card,
   'https://www.figma.com/design/U58Pbb84dLaZfvFvdtGVdT/Comet-UI-Kit?node-id=43-2604',
   {
     props: {},
+    variant: {
+      Variant: 'Default',
+    },
     example: () => (
-      <Card id="card-1">
-        <CardHeader>Card Header</CardHeader>
-        <CardBody>Card Body</CardBody>
+      <Card id="card-default">
+        <CardMedia>
+          <img
+            src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg"
+            alt="a placeholder image"
+          />
+        </CardMedia>
+        <CardHeader>Card title</CardHeader>
+        <CardBody>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum tenetur quo
+          cupiditate, eaque qui officia recusandae.
+        </CardBody>
         <CardFooter>
-          <Button id="card-button" variant="outline">
-            Click me...
-          </Button>
+          <Button id="button">Button</Button>
+        </CardFooter>
+      </Card>
+    ),
+  },
+);
+
+figma.connect(
+  Card,
+  'https://www.figma.com/design/U58Pbb84dLaZfvFvdtGVdT/Comet-UI-Kit?node-id=43-2604',
+  {
+    props: {},
+    variant: {
+      Variant: 'Header first',
+    },
+    example: () => (
+      <Card id="card-default">
+        <CardHeader>Card title</CardHeader>
+        <CardMedia>
+          <img
+            src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg"
+            alt="a placeholder image"
+          />
+        </CardMedia>
+        <CardBody>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum tenetur quo
+          cupiditate, eaque qui officia recusandae.
+        </CardBody>
+        <CardFooter>
+          <Button id="button">Button</Button>
+        </CardFooter>
+      </Card>
+    ),
+  },
+);
+
+figma.connect(
+  Card,
+  'https://www.figma.com/design/U58Pbb84dLaZfvFvdtGVdT/Comet-UI-Kit?node-id=46-7016',
+  {
+    props: {
+      mediaRight: figma.enum('Variant', {
+        'Media left': false,
+        'Media right': true,
+      }),
+    },
+    example: ({ mediaRight }) => (
+      <Card id="card-default" layout="flag" mediaRight={mediaRight}>
+        <CardHeader>Card title</CardHeader>
+        <CardMedia>
+          <img
+            src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg"
+            alt="a placeholder image"
+          />
+        </CardMedia>
+        <CardBody>Lorem ipsum dolor sit amet consectetur adipisicing elit.</CardBody>
+        <CardFooter>
+          <Button id="button">Button</Button>
         </CardFooter>
       </Card>
     ),
