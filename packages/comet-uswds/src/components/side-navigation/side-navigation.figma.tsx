5c9b06010c7cbb8daf86cbbbe6e843981b5ed58b
Johnny Bouder <jbouder@metrostar.com>
2024-08-28 08:48:34 -0400

    Cleanup code connect files for process list and side navigation to provide better code examples.

--- packages/comet-uswds/src/components/side-navigation/side-navigation.figma.tsx
+++ packages/comet-uswds/src/components/side-navigation/side-navigation.figma.tsx
@@ -5,122 +5,151 @@ figma.connect(
   SideNavigation,
   'https://www.figma.com/design/U58Pbb84dLaZfvFvdtGVdT/Comet-UI-Kit?node-id=481-3041',
   {
-    props: {
-      items: figma.enum('Type', {
-        Flat: [
-          {
-            anchor: (
-              <a className="usa-current" href="/" onClick={function Gs() {}}>
-                Navigation Link
-              </a>
-            ),
-          },
-          {
-            anchor: (
-              <a className="" href="/" onClick={function Gs() {}}>
-                Navigation Link
-              </a>
-            ),
-          },
-          {
-            anchor: (
-              <a className="" href="/" onClick={function Gs() {}}>
-                Navigation Link
-              </a>
-            ),
-          },
-        ],
-        'Two level': [
-          {
-            anchor: (
-              <a className="usa-current" href="/" onClick={function Gs() {}}>
-                Navigation Link
-              </a>
-            ),
-            items: [
-              {
-                anchor: (
-                  <a className="" href="/" onClick={function Gs() {}}>
-                    Navigation Link
-                  </a>
-                ),
-              },
-              {
-                anchor: (
-                  <a className="" href="/" onClick={function Gs() {}}>
-                    Navigation Link
-                  </a>
-                ),
-              },
-              {
-                anchor: (
-                  <a className="" href="/" onClick={function Gs() {}}>
-                    Navigation Link
-                  </a>
-                ),
-              },
-              {
-                anchor: (
-                  <a className="usa-current" href="/" onClick={function Gs() {}}>
-                    Navigation Link
-                  </a>
-                ),
-              },
-            ],
-          },
-        ],
-        'Three level': [
-          {
-            anchor: (
-              <a className="usa-current" href="/" onClick={function Gs() {}}>
-                Navigation Link
-              </a>
-            ),
-            items: [
-              {
-                anchor: (
-                  <a className="usa-current" href="/" onClick={function Gs() {}}>
-                    Navigation Link
-                  </a>
-                ),
-                items: [
-                  {
-                    anchor: (
-                      <a className="" href="/" onClick={function Gs() {}}>
-                        Navigation Link
-                      </a>
-                    ),
-                  },
-                  {
-                    anchor: (
-                      <a className="" href="/" onClick={function Gs() {}}>
-                        Navigation Link
-                      </a>
-                    ),
-                  },
-                  {
-                    anchor: (
-                      <a className="" href="/" onClick={function Gs() {}}>
-                        Navigation Link
-                      </a>
-                    ),
-                  },
-                  {
-                    anchor: (
-                      <a className="usa-current" href="/" onClick={function Gs() {}}>
-                        Navigation Link
-                      </a>
-                    ),
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      }),
+    props: {},
+    variant: {
+      Type: 'Flat',
     },
-    example: ({ items }) => (
-      <SideNavigation ariaLabel="Side navigation" id="side-navigation" items={items} />
-    ),
+    example: () => {
+      const items = [
+        {
+          anchor: (
+            <a className="usa-current" href="/" onClick={function Gs() {}}>
+              Navigation Link
+            </a>
+          ),
+        },
+        {
+          anchor: (
+            <a className="" href="/" onClick={function Gs() {}}>
+              Navigation Link
+            </a>
+          ),
+        },
+        {
+          anchor: (
+            <a className="" href="/" onClick={function Gs() {}}>
+              Navigation Link
+            </a>
+          ),
+        },
+      ];
+
+      return <SideNavigation ariaLabel="Side navigation" id="side-navigation" items={items} />;
+    },
+  },
+);
+
+figma.connect(
+  SideNavigation,
+  'https://www.figma.com/design/U58Pbb84dLaZfvFvdtGVdT/Comet-UI-Kit?node-id=481-3041',
+  {
+    props: {},
+    variant: {
+      Type: 'Two level',
+    },
+    example: () => {
+      const items = [
+        {
+          anchor: (
+            <a className="usa-current" href="/" onClick={function Gs() {}}>
+              Navigation Link
+            </a>
+          ),
+          items: [
+            {
+              anchor: (
+                <a className="" href="/" onClick={function Gs() {}}>
+                  Navigation Link
+                </a>
+              ),
+            },
+            {
+              anchor: (
+                <a className="" href="/" onClick={function Gs() {}}>
+                  Navigation Link
+                </a>
+              ),
+            },
+            {
+              anchor: (
+                <a className="" href="/" onClick={function Gs() {}}>
+                  Navigation Link
+                </a>
+              ),
+            },
+            {
+              anchor: (
+                <a className="usa-current" href="/" onClick={function Gs() {}}>
+                  Navigation Link
+                </a>
+              ),
+            },
+          ],
+        },
+      ];
+      return <SideNavigation ariaLabel="Side navigation" id="side-navigation" items={items} />;
+    },
+  },
+);
+
+figma.connect(
+  SideNavigation,
+  'https://www.figma.com/design/U58Pbb84dLaZfvFvdtGVdT/Comet-UI-Kit?node-id=481-3041',
+  {
+    props: {},
+    variant: {
+      Type: 'Three level',
+    },
+    example: () => {
+      const items = [
+        {
+          anchor: (
+            <a className="usa-current" href="/" onClick={function Gs() {}}>
+              Navigation Link
+            </a>
+          ),
+          items: [
+            {
+              anchor: (
+                <a className="usa-current" href="/" onClick={function Gs() {}}>
+                  Navigation Link
+                </a>
+              ),
+              items: [
+                {
+                  anchor: (
+                    <a className="" href="/" onClick={function Gs() {}}>
+                      Navigation Link
+                    </a>
+                  ),
+                },
+                {
+                  anchor: (
+                    <a className="" href="/" onClick={function Gs() {}}>
+                      Navigation Link
+                    </a>
+                  ),
+                },
+                {
+                  anchor: (
+                    <a className="" href="/" onClick={function Gs() {}}>
+                      Navigation Link
+                    </a>
+                  ),
+                },
+                {
+                  anchor: (
+                    <a className="usa-current" href="/" onClick={function Gs() {}}>
+                      Navigation Link
+                    </a>
+                  ),
+                },
+              ],
+            },
+          ],
+        },
+      ];
+      return <SideNavigation ariaLabel="Side navigation" id="side-navigation" items={items} />;
+    },
   },
 );
