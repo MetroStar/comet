ba84cf14f43bec7e9461f10b9a1743a34746eb07
Johnny Bouder <jbouder@metrostar.com>
2024-09-25 09:06:56 -0400

    Fix rollup scripts for comet-extras and comet-data-viz

--- packages/comet-extras/rollup.config.mjs
+++ packages/comet-extras/rollup.config.mjs
@@ -40,7 +40,7 @@ export default [
     },
   },
   {
-    input: './dist/esm/index.d.ts',
+    input: './dist/esm/types/index.d.ts',
     output: [{ file: './dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
     external: [/\.css$/],
