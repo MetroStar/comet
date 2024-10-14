b1d8c7b8d342d5df8e499d6b618eb0e28a276dd3
jbouder <61591423+jbouder@users.noreply.github.com>
2024-10-14 22:35:52 +0000

    Deploying to gh-pages from @ MetroStar/comet@aba0daec91545e29872b002c5bbc6f423ad568e3 🚀

---
+++ assets/in-page-navigation.stories-Ds409Yf_.js
@@ -0,0 +1,31 @@
+import{j as p}from"./jsx-runtime-Bxcv0yjW.js";import{I as r}from"./table-CEz_fbHy.js";import"./banner-C3OBBKoY.js";import"./button-Dij490EH.js";import"./button-group-Jm6xw3dW.js";import"./checkbox-BRLoFrPj.js";import"./combo-box-ImVOlfq6.js";import"./date-picker-nOPe1z9O.js";import"./error-messages-B09LlTSG.js";import"./index-BuiJXcB1.js";import"./form-group-BYOOw0zj.js";import"./helper-text-BGGx8P4c.js";import"./icon-BQnBcIKm.js";import"./label-aifmuN2d.js";import"./modal-CVE53OII.js";import"./process-list-CA9hh0io.js";import"./radio-button-CDY2eEGq.js";import"./range-slider-BZcG2zVs.js";import"./search-DIodcva8.js";import"./select-CoQFGJXr.js";import"./side-navigation-C1wTXBlT.js";import"./site-alert-BiYBHMCS.js";import"./step-indicator-DL-d1Rw4.js";import"./summary-box-BROm9rVe.js";import"./tag-Dg5v4hiQ.js";import"./text-area-BhYGyMaq.js";import"./time-picker-Ck-jU2s-.js";import"./tooltip-aEOz7tM1.js";import"./index-CVDpGf7L.js";import"./index-mfbfsM12.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Bl6ORisp.js";import"./index-BPeBj1-_.js";import"./config-BXli1stc.js";import"./is-in-viewport-B5ZZJcwh.js";import"./events-C6LRLXdn.js";import"./select-or-matches-BLLALKpn.js";import"./sanitizer-BtXph-xc.js";import"./sprite-Cw9lcZJJ.js";import"./index-Da4cx7Kx.js";import"./active-element-CTMAq7ve.js";const X={title:"USWDS/In-Page Navigation",component:r,argTypes:{id:{required:!0}}},i=`
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo,
+    ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio
+    lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula
+    volutpat condimentum. Aliquam erat volutpat. Sed quis velit. Nulla
+    facilisi. Nulla libero. Vivamus pharetra posuere sapien. Lorem ipsum dolor
+    sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra
+    gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis.
+    Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat
+    condimentum. Aliquam erat volutpat. Sed quis velit. Nulla facilisi. Nulla
+    libero. Vivamus pharetra posuere sapien. Lorem ipsum dolor sit amet,
+    consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida,
+    orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit
+    amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam
+    erat volutpat. Sed quis velit. Nulla facilisi. Nulla libero. Vivamus
+    pharetra posuere sapien. Lorem ipsum dolor sit amet, consectetuer
+    adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna
+    rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim.
+    Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat
+    volutpat. Sed quis velit. Nulla facilisi. Nulla libero. Vivamus pharetra
+    posuere sapien. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+    Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id
+    pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit
+    vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit.
+    Nulla facilisi. Nulla libero. Vivamus pharetra posuere sapien. Lorem ipsum
+    dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed
+    pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non
+    turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat
+    condimentum. Aliquam erat volutpat. Sed quis velit. Nulla facilisi. Nulla
+    libero. Vivamus pharetra posuere sapien.
+`,s=m=>p.jsx(r,{...m}),e=s.bind({});e.args={id:"nav-1",navTitle:"On this page",pageTitle:"Sample in-page navigation page",items:[{heading:"Lorem ipsum dolor",content:i,headingType:"h2"},{heading:"Consectetuer adipiscing elit",content:i,headingType:"h2"},{heading:"Nullam sit amet enim",content:i,headingType:"h3"},{heading:"Vivamus pharetra posuere sapien",content:i,headingType:"h3"},{heading:"Suspendisse id velit",content:i,headingType:"h2"}]};var a,t,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:"(args: InPageNavigationProps) => <InPageNavigation {...args} />",...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const Y=["Default"];export{e as Default,Y as __namedExportsOrder,X as default};
