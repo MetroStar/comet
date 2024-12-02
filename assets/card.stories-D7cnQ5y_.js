7da957d6f7c2ee52e5956cfbfc11ce9779226e74
jbouder <61591423+jbouder@users.noreply.github.com>
2024-12-02 23:38:21 +0000

    Deploying to gh-pages from @ MetroStar/comet@d8151bdbbe0ca6bda0c440707d69acf78686b499 🚀

---
+++ assets/card.stories-D7cnQ5y_.js
@@ -0,0 +1,34 @@
+import{j as r}from"./jsx-runtime-DPda6Ybg.js";import{C as o,d as C,e as f,f as h,g as y}from"./table-BDKz0mvD.js";import"./banner-DCaJRUn6.js";import{B}from"./button-BK-sSSZL.js";import"./button-group-COrkmNtR.js";import"./checkbox-Cd8NtStn.js";import"./combo-box-D1gd0ZTz.js";import"./date-picker-DuzZabgW.js";import"./error-messages-BtGU3DT8.js";import"./index-CzvC9lYh.js";import"./form-group-xsfonisk.js";import"./helper-text-B_e6CW13.js";import"./icon-Dugdb9ZQ.js";import"./label-BcV181Ee.js";import"./modal-Db4067mq.js";import"./process-list-Bt090oHO.js";import"./radio-button-BGLOFIKA.js";import"./range-slider-CBdcLky-.js";import"./search-DRPRFrGd.js";import"./select-BSDXGm39.js";import"./side-navigation-Bhmmua-H.js";import"./site-alert-CL7H2pqO.js";import"./step-indicator-CsuLo08T.js";import"./summary-box-05qKTCUx.js";import"./tag-Bwb1sPu4.js";import"./text-area-Bj6JkRlB.js";import"./time-picker-BWR9YJ20.js";import"./tooltip-CPbj5umw.js";import"./index-DTujk8nr.js";import"./index-DhrokLn_.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DUolvyrz.js";import"./index-a0hvGR0d.js";import"./config-BpWtFwiV.js";import"./is-in-viewport-B5ZZJcwh.js";import"./events-C6LRLXdn.js";import"./select-or-matches-C9E_1WAJ.js";import"./sanitizer-BtXph-xc.js";import"./sprite-Cw9lcZJJ.js";import"./index-B7_m26zB.js";import"./active-element-CTMAq7ve.js";const mr={title:"USWDS/Card",component:o,argTypes:{id:{required:!0},layout:{control:{type:"select",options:["default","flag"]}}}},j=e=>r.jsxs(o,{...e,children:[r.jsx(C,{children:"Card"}),r.jsx(f,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum tenetur quo cupiditate, eaque qui officia recusandae."}),r.jsx(h,{children:r.jsx(B,{id:"card-button",children:"Button"})})]}),a=j.bind({});a.args={id:"card-1",layout:"default",className:"maxw-mobile-lg"};const x=e=>r.jsxs(o,{...e,children:[r.jsx(y,{children:r.jsx("img",{src:"https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg",alt:"a placeholder image"})}),r.jsx(C,{children:"Card with Media"}),r.jsx(f,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum tenetur quo cupiditate, eaque qui officia recusandae."}),r.jsx(h,{children:r.jsx(B,{id:"card-button",children:"Button"})})]}),t=x.bind({});t.args={id:"card-standard-media",layout:"default",className:"maxw-mobile"};const i=x.bind({});i.args={id:"card-flag-media",layout:"flag",mediaRight:!1,className:"maxw-tablet"};var d,s,n;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`(args: CardProps) => <Card {...args}>
+    <CardHeader>Card</CardHeader>
+    <CardBody>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum tenetur quo cupiditate,
+      eaque qui officia recusandae.
+    </CardBody>
+    <CardFooter>
+      <Button id="card-button">Button</Button>
+    </CardFooter>
+  </Card>`,...(n=(s=a.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var m,u,c;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`(args: CardProps) => <Card {...args}>
+    <CardMedia>
+      <img src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg" alt="a placeholder image" />
+    </CardMedia>
+    <CardHeader>Card with Media</CardHeader>
+    <CardBody>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum tenetur quo cupiditate,
+      eaque qui officia recusandae.
+    </CardBody>
+    <CardFooter>
+      <Button id="card-button">Button</Button>
+    </CardFooter>
+  </Card>`,...(c=(u=t.parameters)==null?void 0:u.docs)==null?void 0:c.source}}};var p,l,g;i.parameters={...i.parameters,docs:{...(p=i.parameters)==null?void 0:p.docs,source:{originalSource:`(args: CardProps) => <Card {...args}>
+    <CardMedia>
+      <img src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg" alt="a placeholder image" />
+    </CardMedia>
+    <CardHeader>Card with Media</CardHeader>
+    <CardBody>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum tenetur quo cupiditate,
+      eaque qui officia recusandae.
+    </CardBody>
+    <CardFooter>
+      <Button id="card-button">Button</Button>
+    </CardFooter>
+  </Card>`,...(g=(l=i.parameters)==null?void 0:l.docs)==null?void 0:g.source}}};const ur=["Default","WithMedia","FlagLayout"];export{a as Default,i as FlagLayout,t as WithMedia,ur as __namedExportsOrder,mr as default};
