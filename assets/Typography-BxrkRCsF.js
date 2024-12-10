0b96ff87be5a6739eeacaeb62bf498a6cc387fc2
jbouder <61591423+jbouder@users.noreply.github.com>
2024-12-10 13:59:05 +0000

    Deploying to gh-pages from @ MetroStar/comet@417adc3e39bb4f3ae8c2725aadaf3069c997e924 🚀

--- assets/Typography-BxrkRCsF.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-BjgbQsUx.js";import{useMDXComponents as i}from"./index--DeVyQia.js";import{ae as n,ah as m}from"./index-CO1sm7sn.js";import"./index-D2MAbzvX.js";import"./index-DDT2H6na.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-B6NHWqVV.js";import"../sb-preview/runtime.js";import"./index-BIm0odtm.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";const e={type:{primary:"'Source Sans Pro','Helvetica Neue', 'Helvetica', 'Roboto', 'Arial', sans-serif"},weight:{light:"300",normal:"400",bold:"700"},size:{"3xs":12,"2xs":14,xs:16,sm:17,md:18,lg:24,xl:32,"2xl":40,"3xl":56}},p="Lorem ipsum dolor sit amet, consectetur adipiscing elit.";function o(s){const r={h1:"h1",p:"p",strong:"strong",...i(),...s.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{title:"Style Guide/Typography"}),`
-`,`
-`,`
-`,t.jsx(r.h1,{id:"typography",children:"Typography"}),`
-`,t.jsxs(r.p,{children:[t.jsx(r.strong,{children:"Font:"})," Source Sans Pro"]}),`
-`,t.jsxs(r.p,{children:[t.jsx(r.strong,{children:"Weights:"})," 300(light), 400(normal), 700(bold)"]}),`
-`,t.jsx(m,{fontSizes:[Number(e.size["3xs"]),Number(e.size["2xs"]),Number(e.size.xs),Number(e.size.sm),Number(e.size.md),Number(e.size.lg),Number(e.size.xl),Number(e.size["2xl"]),Number(e.size["3xl"])],fontWeight:e.weight.normal,sampleText:p,fontFamily:e.type.primary})]})}function f(s={}){const{wrapper:r}={...i(),...s.components};return r?t.jsx(r,{...s,children:t.jsx(o,{...s})}):o(s)}export{p as SampleText,f as default,e as typography};
---
+++ assets/in-page-navigation.stories-F_NB-PSb.js
@@ -0,0 +1,31 @@
+import{j as p}from"./jsx-runtime-BjgbQsUx.js";import{I as r}from"./table-D0czvN2b.js";import"./banner-Dheu0Y1C.js";import"./button-DW-w6e0V.js";import"./button-group-BmlUvBcA.js";import"./checkbox-p9BQCb34.js";import"./combo-box-BN6vvru3.js";import"./date-picker-Cer0hSHi.js";import"./error-messages-9EHEOSno.js";import"./index-BllMWJqc.js";import"./form-group-CsH8EhgF.js";import"./helper-text-CvsfwBJV.js";import"./icon-OgzK0umJ.js";import"./label-Cm37mQmh.js";import"./modal-BsyfNARE.js";import"./process-list-Djj0IDFY.js";import"./radio-button-BFoI3scR.js";import"./range-slider-x_RP9ET-.js";import"./search-C8b_qcG2.js";import"./select-Dj6TyvHu.js";import"./side-navigation-BOOTbG8P.js";import"./site-alert-BSE1Mwml.js";import"./step-indicator-D9rdBPax.js";import"./summary-box-lJlgPCIg.js";import"./tag-CeVjYVBv.js";import"./text-area-CKO3fHqq.js";import"./time-picker-rf2kBXRQ.js";import"./tooltip-eIpy4DRV.js";import"./index-B10PtKfh.js";import"./index-D2MAbzvX.js";import"./index-DDT2H6na.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DFtWylL1.js";import"./config-jVN7-cmM.js";import"./select-or-matches-Ry_FXvm3.js";import"./events-CleOcHg4.js";import"./sanitizer-BDKKlE4O.js";import"./index-ULqyV3G8.js";import"./is-in-viewport-ChIQrv3L.js";import"./sprite-Cw9lcZJJ.js";import"./index-BKt2X7Zg.js";import"./active-element-BuxAly_Z.js";const Y={title:"USWDS/In-Page Navigation",component:r,argTypes:{id:{required:!0}}},i=`
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo,
+    ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio
+    lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula
+    volutpat condimentum. Aliquam erat volutpat. Sed quis velit. Nulla
+    facilisi. Nulla libero. Vivamus pharetra posuere sapien. Lorem ipsum dolor
+    sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra
+    gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis.
+    Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat
+    condimentum. Aliquam erat volutpat. Sed quis velit. Nulla facilisi. Nulla
+    libero. Vivamus pharetra posuere sapien. Lorem ipsum dolor sit amet,
+    consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida,
+    orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit
+    amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam
+    erat volutpat. Sed quis velit. Nulla facilisi. Nulla libero. Vivamus
+    pharetra posuere sapien. Lorem ipsum dolor sit amet, consectetuer
+    adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna
+    rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim.
+    Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat
+    volutpat. Sed quis velit. Nulla facilisi. Nulla libero. Vivamus pharetra
+    posuere sapien. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+    Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id
+    pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit
+    vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit.
+    Nulla facilisi. Nulla libero. Vivamus pharetra posuere sapien. Lorem ipsum
+    dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed
+    pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non
+    turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat
+    condimentum. Aliquam erat volutpat. Sed quis velit. Nulla facilisi. Nulla
+    libero. Vivamus pharetra posuere sapien.
+`,s=m=>p.jsx(r,{...m}),e=s.bind({});e.args={id:"nav-1",navTitle:"On this page",pageTitle:"Sample in-page navigation page",items:[{heading:"Lorem ipsum dolor",content:i,headingType:"h2"},{heading:"Consectetuer adipiscing elit",content:i,headingType:"h2"},{heading:"Nullam sit amet enim",content:i,headingType:"h3"},{heading:"Vivamus pharetra posuere sapien",content:i,headingType:"h3"},{heading:"Suspendisse id velit",content:i,headingType:"h2"}]};var a,t,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:"(args: InPageNavigationProps) => <InPageNavigation {...args} />",...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const Z=["Default"];export{e as Default,Z as __namedExportsOrder,Y as default};
