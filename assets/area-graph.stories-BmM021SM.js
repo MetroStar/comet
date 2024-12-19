74e20a7d86daa6545fb02881443da677b36b70c0
jbouder <61591423+jbouder@users.noreply.github.com>
2024-12-19 20:06:13 +0000

    Deploying to gh-pages from @ MetroStar/comet@5e0ba141df0c359e647f92543db0cb29ea5daf3d 🚀

---
+++ assets/area-graph.stories-BmM021SM.js
@@ -0,0 +1,6 @@
+import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{A as s}from"./stack-graph-CTxTW7CD.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";const m={title:"Data Viz/Area Graph",component:s,argTypes:{smooth:{control:"boolean"}}},x=p=>a.jsx("div",{style:{width:"600px",height:"400px"},children:a.jsx(s,{...p})}),r=x.bind({});r.args={chart:{title:"Area graph",width:600,height:400},colors:["#0d7ea2","#cd425b"],data:[[{x:0,y:3},{x:1,y:9},{x:2,y:5},{x:3,y:12},{x:4,y:6},{x:5,y:10}],[{x:0,y:1},{x:1,y:4},{x:2,y:7},{x:3,y:2},{x:4,y:5},{x:5,y:7}]],smooth:!1};var t,e,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`(args: AreaGraphProps) => <div style={{
+  width: '600px',
+  height: '400px'
+}}>
+    <AreaGraph {...args} />
+  </div>`,...(o=(e=r.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const c=["Default"];export{r as Default,c as __namedExportsOrder,m as default};
