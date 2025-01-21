d2730eb2b6e9ff0329ef1d77a1f714c60a2ea4ba
jbouder <61591423+jbouder@users.noreply.github.com>
2025-01-21 14:08:10 +0000

    Deploying to gh-pages from @ MetroStar/comet@256f72bace66adb33c947a15e49848d575a1aa8e 🚀

---
+++ assets/card.stories-bcXb7ZRK.js
@@ -0,0 +1,34 @@
+import{j as r}from"./jsx-runtime-CLpGMVip.js";import{C as o,d as C,e as f,f as h,g as y}from"./table-BeJ80pMB.js";import"./banner-C4UUz6yC.js";import{B}from"./button-lXZ9EEgl.js";import"./button-group-DfEF9I7-.js";import"./checkbox-C_UJYIYS.js";import"./combo-box-BJIAMHTU.js";import"./date-picker-DgOn0wN-.js";import"./error-messages-BWeax-Kj.js";import"./index-CNmeIYsO.js";import"./form-group-Bu8kLN8Y.js";import"./helper-text-BmhHZMwR.js";import"./icon-Cbl3A-UV.js";import"./label-BvRQRArz.js";import"./modal-BmJpwfRh.js";import"./process-list-C40Zpa_8.js";import"./radio-button-kqPTm1v1.js";import"./range-slider-CcnhuTdI.js";import"./search-DeWPXCtO.js";import"./select-BU_Z1_VN.js";import"./side-navigation-DinQtbKC.js";import"./site-alert-2OQ5RSaS.js";import"./step-indicator-Dugk_wpx.js";import"./summary-box-BjmG18Sb.js";import"./tag-QqKs67IJ.js";import"./text-area-BjhCSdHX.js";import"./time-picker-X66lcLNk.js";import"./tooltip-ee6nW_ea.js";import"./index-B10PtKfh.js";import"./index-D7xSkP6m.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DFtWylL1.js";import"./config-jVN7-cmM.js";import"./select-or-matches-Ry_FXvm3.js";import"./events-CleOcHg4.js";import"./sanitizer-BDKKlE4O.js";import"./index-ULqyV3G8.js";import"./is-in-viewport-ChIQrv3L.js";import"./sprite-Cw9lcZJJ.js";import"./index-BKt2X7Zg.js";import"./active-element-BuxAly_Z.js";const mr={title:"USWDS/Card",component:o,argTypes:{id:{required:!0},layout:{control:{type:"select",options:["default","flag"]}}}},j=e=>r.jsxs(o,{...e,children:[r.jsx(C,{children:"Card"}),r.jsx(f,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum tenetur quo cupiditate, eaque qui officia recusandae."}),r.jsx(h,{children:r.jsx(B,{id:"card-button",children:"Button"})})]}),a=j.bind({});a.args={id:"card-1",layout:"default",className:"maxw-mobile-lg"};const x=e=>r.jsxs(o,{...e,children:[r.jsx(y,{children:r.jsx("img",{src:"https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg",alt:"a placeholder image"})}),r.jsx(C,{children:"Card with Media"}),r.jsx(f,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum tenetur quo cupiditate, eaque qui officia recusandae."}),r.jsx(h,{children:r.jsx(B,{id:"card-button",children:"Button"})})]}),t=x.bind({});t.args={id:"card-standard-media",layout:"default",className:"maxw-mobile"};const i=x.bind({});i.args={id:"card-flag-media",layout:"flag",mediaRight:!1,className:"maxw-tablet"};var d,s,n;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`(args: CardProps) => <Card {...args}>
+    <CardHeader>Card</CardHeader>
+    <CardBody>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum tenetur quo cupiditate,
+      eaque qui officia recusandae.
+    </CardBody>
+    <CardFooter>
+      <Button id="card-button">Button</Button>
+    </CardFooter>
+  </Card>`,...(n=(s=a.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var m,u,c;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`(args: CardProps) => <Card {...args}>
+    <CardMedia>
+      <img src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg" alt="a placeholder image" />
+    </CardMedia>
+    <CardHeader>Card with Media</CardHeader>
+    <CardBody>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum tenetur quo cupiditate,
+      eaque qui officia recusandae.
+    </CardBody>
+    <CardFooter>
+      <Button id="card-button">Button</Button>
+    </CardFooter>
+  </Card>`,...(c=(u=t.parameters)==null?void 0:u.docs)==null?void 0:c.source}}};var p,l,g;i.parameters={...i.parameters,docs:{...(p=i.parameters)==null?void 0:p.docs,source:{originalSource:`(args: CardProps) => <Card {...args}>
+    <CardMedia>
+      <img src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg" alt="a placeholder image" />
+    </CardMedia>
+    <CardHeader>Card with Media</CardHeader>
+    <CardBody>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum tenetur quo cupiditate,
+      eaque qui officia recusandae.
+    </CardBody>
+    <CardFooter>
+      <Button id="card-button">Button</Button>
+    </CardFooter>
+  </Card>`,...(g=(l=i.parameters)==null?void 0:l.docs)==null?void 0:g.source}}};const ur=["Default","WithMedia","FlagLayout"];export{a as Default,i as FlagLayout,t as WithMedia,ur as __namedExportsOrder,mr as default};
---
+++ assets/table.stories-BxnQ9jVZ.js
@@ -0,0 +1,253 @@
+import{j as V}from"./jsx-runtime-CLpGMVip.js";import{l as d}from"./table-BeJ80pMB.js";import"./banner-C4UUz6yC.js";import"./button-lXZ9EEgl.js";import"./button-group-DfEF9I7-.js";import"./checkbox-C_UJYIYS.js";import"./combo-box-BJIAMHTU.js";import"./date-picker-DgOn0wN-.js";import"./error-messages-BWeax-Kj.js";import"./index-CNmeIYsO.js";import"./form-group-Bu8kLN8Y.js";import"./helper-text-BmhHZMwR.js";import"./icon-Cbl3A-UV.js";import"./label-BvRQRArz.js";import"./modal-BmJpwfRh.js";import"./process-list-C40Zpa_8.js";import"./radio-button-kqPTm1v1.js";import"./range-slider-CcnhuTdI.js";import"./search-DeWPXCtO.js";import"./select-BU_Z1_VN.js";import"./side-navigation-DinQtbKC.js";import"./site-alert-2OQ5RSaS.js";import"./step-indicator-Dugk_wpx.js";import"./summary-box-BjmG18Sb.js";import"./tag-QqKs67IJ.js";import"./text-area-BjhCSdHX.js";import"./time-picker-X66lcLNk.js";import"./tooltip-ee6nW_ea.js";import"./index-B10PtKfh.js";import"./index-D7xSkP6m.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DFtWylL1.js";import"./config-jVN7-cmM.js";import"./select-or-matches-Ry_FXvm3.js";import"./events-CleOcHg4.js";import"./sanitizer-BDKKlE4O.js";import"./index-ULqyV3G8.js";import"./is-in-viewport-ChIQrv3L.js";import"./sprite-Cw9lcZJJ.js";import"./index-BKt2X7Zg.js";import"./active-element-BuxAly_Z.js";const te={title:"USWDS/Table",component:d,argTypes:{id:{required:!0},sortDir:{control:"select"}}},u=e=>{const m=[{id:"name",name:"Name"},{id:"orderAdmitted",name:"Order admitted to union"},{id:"dateOfVote",name:"Date of ratification vote"},{id:"dateAdmitted",name:"Date admitted to union"},{id:"percentVoters",name:"Percent of voters in favor of ratification"},{id:"votesCast",name:"Votes cast in favor of ratification"},{id:"estimatedPopulation",name:"Estimated population at time of admission",sortable:!1}],p=[{name:{value:"Hawaii",sortValue:"Hawaii"},orderAdmitted:{value:"50th",sortValue:"50"},dateOfVote:{value:"Apr. 24, 1956",sortValue:"331844400"},dateAdmitted:{value:"Aug. 21, 1959",sortValue:"327092400"},percentVoters:{value:"94.3%",sortValue:"0.943"},votesCast:{value:"132,773",sortValue:"132773"},estimatedPopulation:{value:"632,772",sortValue:"632772"}},{name:{value:"Alaska",sortValue:"Alaska"},orderAdmitted:{value:"49th",sortValue:"49"},dateOfVote:{value:"Jun. 27, 1959",sortValue:"431978400"},dateAdmitted:{value:"Jan. 3, 1959",sortValue:"346960800"},percentVoters:{value:"68.1%",sortValue:"0.681"},votesCast:{value:"17,477",sortValue:"17477"},estimatedPopulation:{value:"226,167",sortValue:"226167"}},{name:{value:"Arizona",sortValue:"Arizona"},orderAdmitted:{value:"48th",sortValue:"48"},dateOfVote:{value:"Feb. 9, 1911",sortValue:"1858528800"},dateAdmitted:{value:"Feb. 14, 1912",sortValue:"1826560800"},percentVoters:{value:"78.7%",sortValue:"0.787"},votesCast:{value:"12,187",sortValue:"12187"},estimatedPopulation:{value:"204,354",sortValue:"204354"}}];return V.jsx(d,{id:e.id,tabIndex:e.tabIndex,columns:m,data:p,caption:e.caption,borderless:e.borderless,striped:e.striped,scrollable:e.scrollable,sortable:e.sortable,sortDir:e.sortDir,sortIndex:e.sortIndex,onSort:e.onSort})},t=u.bind({});t.args={id:"table-1",tabIndex:1,caption:"Voter Data",borderless:!1,striped:!1,scrollable:!1,sortable:!0,sortDir:"ascending",sortIndex:0};const a=u.bind({});a.args={id:"table-1",tabIndex:1,caption:"Voter Data",borderless:!1,striped:!1,scrollable:!1,sortable:!0,sortDir:"ascending",sortIndex:0,onSort:()=>{console.log("Sorting...")}};var n,o,r;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`(args: TableProps) => {
+  const columns: TableColumn[] = [{
+    id: 'name',
+    name: 'Name'
+  }, {
+    id: 'orderAdmitted',
+    name: 'Order admitted to union'
+  }, {
+    id: 'dateOfVote',
+    name: 'Date of ratification vote'
+  }, {
+    id: 'dateAdmitted',
+    name: 'Date admitted to union'
+  }, {
+    id: 'percentVoters',
+    name: 'Percent of voters in favor of ratification'
+  }, {
+    id: 'votesCast',
+    name: 'Votes cast in favor of ratification'
+  }, {
+    id: 'estimatedPopulation',
+    name: 'Estimated population at time of admission',
+    sortable: false
+  }];
+  interface StateData {
+    name: SortableDataCell | unknown;
+    orderAdmitted: SortableDataCell | unknown;
+    dateOfVote: SortableDataCell | unknown;
+    dateAdmitted: SortableDataCell | unknown;
+    percentVoters: SortableDataCell | unknown;
+    votesCast: SortableDataCell | unknown;
+    estimatedPopulation: SortableDataCell | unknown;
+  }
+  interface SortableDataCell {
+    value: unknown;
+    sortValue: unknown;
+  }
+  const data: StateData[] = [{
+    name: {
+      value: 'Hawaii',
+      sortValue: 'Hawaii'
+    },
+    orderAdmitted: {
+      value: '50th',
+      sortValue: '50'
+    },
+    dateOfVote: {
+      value: 'Apr. 24, 1956',
+      sortValue: '331844400'
+    },
+    dateAdmitted: {
+      value: 'Aug. 21, 1959',
+      sortValue: '327092400'
+    },
+    percentVoters: {
+      value: '94.3%',
+      sortValue: '0.943'
+    },
+    votesCast: {
+      value: '132,773',
+      sortValue: '132773'
+    },
+    estimatedPopulation: {
+      value: '632,772',
+      sortValue: '632772'
+    }
+  }, {
+    name: {
+      value: 'Alaska',
+      sortValue: 'Alaska'
+    },
+    orderAdmitted: {
+      value: '49th',
+      sortValue: '49'
+    },
+    dateOfVote: {
+      value: 'Jun. 27, 1959',
+      sortValue: '431978400'
+    },
+    dateAdmitted: {
+      value: 'Jan. 3, 1959',
+      sortValue: '346960800'
+    },
+    percentVoters: {
+      value: '68.1%',
+      sortValue: '0.681'
+    },
+    votesCast: {
+      value: '17,477',
+      sortValue: '17477'
+    },
+    estimatedPopulation: {
+      value: '226,167',
+      sortValue: '226167'
+    }
+  }, {
+    name: {
+      value: 'Arizona',
+      sortValue: 'Arizona'
+    },
+    orderAdmitted: {
+      value: '48th',
+      sortValue: '48'
+    },
+    dateOfVote: {
+      value: 'Feb. 9, 1911',
+      sortValue: '1858528800'
+    },
+    dateAdmitted: {
+      value: 'Feb. 14, 1912',
+      sortValue: '1826560800'
+    },
+    percentVoters: {
+      value: '78.7%',
+      sortValue: '0.787'
+    },
+    votesCast: {
+      value: '12,187',
+      sortValue: '12187'
+    },
+    estimatedPopulation: {
+      value: '204,354',
+      sortValue: '204354'
+    }
+  }];
+  return <Table id={args.id} tabIndex={args.tabIndex} columns={columns} data={data} caption={args.caption} borderless={args.borderless} striped={args.striped} scrollable={args.scrollable} sortable={args.sortable} sortDir={args.sortDir} sortIndex={args.sortIndex} onSort={args.onSort} />;
+}`,...(r=(o=t.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};var l,s,i;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`(args: TableProps) => {
+  const columns: TableColumn[] = [{
+    id: 'name',
+    name: 'Name'
+  }, {
+    id: 'orderAdmitted',
+    name: 'Order admitted to union'
+  }, {
+    id: 'dateOfVote',
+    name: 'Date of ratification vote'
+  }, {
+    id: 'dateAdmitted',
+    name: 'Date admitted to union'
+  }, {
+    id: 'percentVoters',
+    name: 'Percent of voters in favor of ratification'
+  }, {
+    id: 'votesCast',
+    name: 'Votes cast in favor of ratification'
+  }, {
+    id: 'estimatedPopulation',
+    name: 'Estimated population at time of admission',
+    sortable: false
+  }];
+  interface StateData {
+    name: SortableDataCell | unknown;
+    orderAdmitted: SortableDataCell | unknown;
+    dateOfVote: SortableDataCell | unknown;
+    dateAdmitted: SortableDataCell | unknown;
+    percentVoters: SortableDataCell | unknown;
+    votesCast: SortableDataCell | unknown;
+    estimatedPopulation: SortableDataCell | unknown;
+  }
+  interface SortableDataCell {
+    value: unknown;
+    sortValue: unknown;
+  }
+  const data: StateData[] = [{
+    name: {
+      value: 'Hawaii',
+      sortValue: 'Hawaii'
+    },
+    orderAdmitted: {
+      value: '50th',
+      sortValue: '50'
+    },
+    dateOfVote: {
+      value: 'Apr. 24, 1956',
+      sortValue: '331844400'
+    },
+    dateAdmitted: {
+      value: 'Aug. 21, 1959',
+      sortValue: '327092400'
+    },
+    percentVoters: {
+      value: '94.3%',
+      sortValue: '0.943'
+    },
+    votesCast: {
+      value: '132,773',
+      sortValue: '132773'
+    },
+    estimatedPopulation: {
+      value: '632,772',
+      sortValue: '632772'
+    }
+  }, {
+    name: {
+      value: 'Alaska',
+      sortValue: 'Alaska'
+    },
+    orderAdmitted: {
+      value: '49th',
+      sortValue: '49'
+    },
+    dateOfVote: {
+      value: 'Jun. 27, 1959',
+      sortValue: '431978400'
+    },
+    dateAdmitted: {
+      value: 'Jan. 3, 1959',
+      sortValue: '346960800'
+    },
+    percentVoters: {
+      value: '68.1%',
+      sortValue: '0.681'
+    },
+    votesCast: {
+      value: '17,477',
+      sortValue: '17477'
+    },
+    estimatedPopulation: {
+      value: '226,167',
+      sortValue: '226167'
+    }
+  }, {
+    name: {
+      value: 'Arizona',
+      sortValue: 'Arizona'
+    },
+    orderAdmitted: {
+      value: '48th',
+      sortValue: '48'
+    },
+    dateOfVote: {
+      value: 'Feb. 9, 1911',
+      sortValue: '1858528800'
+    },
+    dateAdmitted: {
+      value: 'Feb. 14, 1912',
+      sortValue: '1826560800'
+    },
+    percentVoters: {
+      value: '78.7%',
+      sortValue: '0.787'
+    },
+    votesCast: {
+      value: '12,187',
+      sortValue: '12187'
+    },
+    estimatedPopulation: {
+      value: '204,354',
+      sortValue: '204354'
+    }
+  }];
+  return <Table id={args.id} tabIndex={args.tabIndex} columns={columns} data={data} caption={args.caption} borderless={args.borderless} striped={args.striped} scrollable={args.scrollable} sortable={args.sortable} sortDir={args.sortDir} sortIndex={args.sortIndex} onSort={args.onSort} />;
+}`,...(i=(s=a.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};const ae=["Default","OnSort"];export{t as Default,a as OnSort,ae as __namedExportsOrder,te as default};
