127b406fb4c1c9c2890e0f9f0727f8211d7eb77a
Johnny Bouder <jbouder@metrostar.com>
2025-06-06 09:26:56 -0400

    Add script for running just unit tests.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   test: {
+    name: 'unit',
     globals: true,
     environment: 'jsdom',
     setupFiles: ['vitest.setup.ts'],
