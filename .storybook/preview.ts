10848f6d568119c8d8c07ea9e6479ba425177695
Johnny Bouder <jbouder@metrostar.com>
2025-06-09 09:56:30 -0400

    Update storybook configs to fail for accessibility. Add storybook test step to workflow.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -27,10 +27,7 @@ const preview = {
     },
 
     a11y: {
-      // 'todo' - show a11y violations in the test UI only
-      // 'error' - fail CI on a11y violations
-      // 'off' - skip a11y checks entirely
-      test: 'todo',
+      test: 'error',
     },
   },
 
