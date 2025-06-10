09e3290f84e1ed7b12ce68d8b9acd693c43e4e60
Johnny Bouder <jbouder@metrostar.com>
2025-06-09 20:27:33 -0400

    Remove console log.

--- packages/comet-uswds/src/components/table/table.tsx
+++ packages/comet-uswds/src/components/table/table.tsx
@@ -102,12 +102,7 @@ export const Table = ({
     const tableElement = tableRef.current;
     // If sortable, call table.on to enable functionality
     if (sortable) {
-      // Check for page locale to ensure proper sorting behavior
-      const locale = navigator.language;
-      console.log(`Table locale: ${locale}`);
-      if (locale) {
-        table.on(tableElement);
-      }
+      table.on(tableElement);
     }
 
     // Ensure cleanup after the effect
