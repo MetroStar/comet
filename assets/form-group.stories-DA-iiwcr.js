b38865abb141f2522badabe52be4dd010d45e58a
bapplejax <jake.bapple@gmail.com>
2026-09-01 14:36:41 +0000

    Deploying to gh-pages from @ MetroStar/comet@f3e393916ca9c6bf6b588cab08bdbd1ae80e72c2 🚀

---
+++ assets/form-group.stories-DA-iiwcr.js
@@ -0,0 +1,22 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{R as t}from"./iframe-B3Hpjk5s.js";import{t as n}from"./jsx-runtime-CaZkqeYb.js";import{m as r,t as i}from"./src-CDBKFXBA.js";import{t as a}from"./label-BM3qvwUp.js";import{n as o,r as s}from"./form-group-Dt9sdDC1.js";import{t as c}from"./helper-text-DEUt-pT9.js";import{t as l}from"./error-messages-B6Ve8llH.js";var u,d,f,p,m;e((()=>{t(),s(),i(),u=n(),d={title:`USWDS/Forms/Form Group`,component:o},f={args:{id:`form-group-1`},render:e=>(0,u.jsxs)(o,{id:e.id,errors:e.errors,children:[(0,u.jsx)(a,{htmlFor:`input1`,children:`Name`}),(0,u.jsx)(c,{children:`Enter your full name`}),(0,u.jsx)(l,{errors:e.errors}),(0,u.jsx)(r,{id:`input1`,name:`input1`})]})},p={args:{id:`form-group-2`,errors:`some error`},render:e=>(0,u.jsxs)(o,{id:e.id,errors:e.errors,children:[(0,u.jsx)(a,{htmlFor:`input1`,children:`Name`}),(0,u.jsx)(c,{children:`Enter your full name`}),(0,u.jsx)(l,{errors:e.errors}),(0,u.jsx)(r,{id:`input1`,name:`input1`})]})},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    id: 'form-group-1'
+  },
+  render: (args: FormGroupProps) => <FormGroup id={args.id} errors={args.errors}>
+      <Label htmlFor="input1">Name</Label>
+      <HelperText>Enter your full name</HelperText>
+      <ErrorMessages errors={args.errors} />
+      <TextInput id="input1" name="input1" />
+    </FormGroup>
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    id: 'form-group-2',
+    errors: 'some error'
+  },
+  render: (args: FormGroupProps) => <FormGroup id={args.id} errors={args.errors}>
+      <Label htmlFor="input1">Name</Label>
+      <HelperText>Enter your full name</HelperText>
+      <ErrorMessages errors={args.errors} />
+      <TextInput id="input1" name="input1" />
+    </FormGroup>
+}`,...p.parameters?.docs?.source}}},m=[`Standard`,`WithErrors`]}))();export{f as Standard,p as WithErrors,m as __namedExportsOrder,d as default};
